e7dec23b813066405a0b19f1374e836313a52eda
Yale Yuen <yaleyyuen@gmail.com>
2016-04-25 17:43:20 -0700

    compelte addQuestion

--- public/app.js
+++ public/app.js
@@ -2,23 +2,23 @@ var quizApp = angular.module('quizApp', []);
 
 quizApp.controller('MainController', ['$scope', function($scope) {
 
-  $scope.quiz = [
-    {'content': 'What is the answer?', 
+  $scope.quiz = [{'content': 'What is the answer?', 
       'option' : { 'a': 0, 'b': 1, 'c': 2, 'd': 3 }, 
     },
     {'content': 'How many question\'s are there?', 
       'option' : { 'a': 3, 'b': 2, 'c': 1, 'd': 0 }
-    } 
-  ];
+    }];
+  $scope.question = {};
 
   $scope.addQuestion = function() {
-    //todo 
-    //should append question to end of current questions
-    angular.element('.test').append();
     
+    $scope.quiz.push({'content': this.content, 'option' : { 'a': this.aVal, 'b': this.bVal, 'c': this.cVal, 'd': this.dVal }});    
+    $scope.content = "";
+    $scope.aVal = "";
+    $scope.bVal = "";
+    $scope.cVal = "";
+    $scope.dVal = "";
   };
-
-
 
 
 }]);
